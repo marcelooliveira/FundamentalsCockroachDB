2208bd325e781e6123067f7aa4ab0e79e3fc1141
marcelo oliveira <marcelo.r.oliveira@ebradi.com.br>
2021-10-05 14:32:22 -0300

    flask-bootstrap

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
